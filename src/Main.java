eea1b4df557cc8d210ea7f8d66c74c72f46ce5b9
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-10-31 12:41:41 -0400

    set up the letters and numbers

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    static String[] letters = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+    static String[] numbers = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String coded = in.next();
+        in.close();
+    }
+}
