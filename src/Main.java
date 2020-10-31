f029e5729bc2ef4454db6bfa98a4975fd17d4509
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-10-31 12:45:47 -0400

    added pain :)

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ class Main{
     static String[] letters = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
 ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
     static String[] numbers = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
-
+    static Set<String> possible = new HashSet<String>();
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         String coded = in.next();
+        pain(coded);
         in.close();
+    }
+    public static boolean pain(String coded){
+        if(coded.length() == 0){
+            return true;
+        }else{
+            //TODO: loop through letters and numbers, then do recursive calls to find stuff and hope DFS is enough
+        }
+        return false;
     }
 }
