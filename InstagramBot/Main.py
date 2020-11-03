20bdcc85606bdc6c0962789a7169c1a032b96818
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-11-03 00:40:21 -0500

    wack ok

--- InstagramBot/Main.py
+++ InstagramBot/Main.py
@@ -2,4 +2,5 @@ import pyautogui, time
 time.sleep(5) # some time to get to the tab
 pyautogui.scroll(-100000) # scrolling is functional
 coords = pyautogui.locateCenterOnScreen('likebtn.png')
+print(coords)
 pyautogui.click(coords)
