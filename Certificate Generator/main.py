e605a8e955ef8aaaab2d8bf5ae70d9e862f82db6
Nathan Lu <nathanymlu@gmail.com>
2024-12-15 17:51:40 -0500

    windows version has comic.ttf instead of Comic Sans MS.ttf

--- Certificate Generator/main.py
+++ Certificate Generator/main.py
@@ -15,14 +15,14 @@ print(img.size)
 
 surface = ImageDraw.Draw(img)
 
-font = ImageFont.truetype("Comic Sans MS.ttf", img.size[1]//len(name))
+font = ImageFont.truetype("comic.ttf", img.size[1]//len(name))
 surface.text((img.size[0]//2, 300), name, font=font, fill=(0, 0, 255), anchor="mm")
 
-font = ImageFont.truetype("Comic Sans MS.ttf", img.size[1]//len(task))
+font = ImageFont.truetype("comic.ttf", img.size[1]//len(task))
 surface.text((img.size[0]//2, 450), task, font=font, fill=(0, 0, 255), anchor="mm")
 
 print(today.day, today.month, today.year)
-font = ImageFont.truetype("Comic Sans MS.ttf", 30)
+font = ImageFont.truetype("comic.ttf", 30)
 surface.text((480, 550), str(today.day), font=font, fill=(0, 0, 255), anchor="lm")
 surface.text((600, 550), today.strftime("%B"), font=font, fill=(0, 0, 255), anchor="lm")
 surface.text((600, 620), str(today.year), font=font, fill=(0, 0, 255), anchor="lm")
