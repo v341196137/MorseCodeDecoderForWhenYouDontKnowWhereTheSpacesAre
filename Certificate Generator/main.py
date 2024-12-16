5db8944fcf31bfe1bbc1b1a8c228c6d898faa507
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2024-12-16 15:40:07 -0500

    made single script again

--- Certificate Generator/main.py
+++ Certificate Generator/main.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 import sys
 import datetime
+import os
 
 print(sys.argv)
 
@@ -11,18 +12,20 @@ today = datetime.date.today()
 
 img = Image.open("./certificate.jpg")
 
+font_name = "Comic Sans MS.ttf" if os.name == "posix" else "comic.ttf"
+
 print(img.size)
 
 surface = ImageDraw.Draw(img)
 
-font = ImageFont.truetype("Comic Sans MS.ttf", img.size[1]//len(name))
+font = ImageFont.truetype(font_name, img.size[1]//len(name))
 surface.text((img.size[0]//2, 300), name, font=font, fill=(0, 0, 255), anchor="mm")
 
-font = ImageFont.truetype("Comic Sans MS.ttf", img.size[1]//len(task))
+font = ImageFont.truetype(font_name, img.size[1]//len(task))
 surface.text((img.size[0]//2, 450), task, font=font, fill=(0, 0, 255), anchor="mm")
 
 print(today.day, today.month, today.year)
-font = ImageFont.truetype("Comic Sans MS.ttf", 30)
+font = ImageFont.truetype(font_name, 30)
 surface.text((480, 550), str(today.day), font=font, fill=(0, 0, 255), anchor="lm")
 surface.text((600, 550), today.strftime("%B"), font=font, fill=(0, 0, 255), anchor="lm")
 surface.text((600, 620), str(today.year), font=font, fill=(0, 0, 255), anchor="lm")
